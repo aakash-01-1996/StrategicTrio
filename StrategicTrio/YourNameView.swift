fcaa4ea99ccdac1b9ba410e5b7c832e10a477551
aakash-01-1996 <aakashambodkar99@gmail.com>
2024-08-04 19:33:38 -0400

    Challenge Device Completed

---
+++ StrategicTrio/YourNameView.swift
@@ -0,0 +1,18 @@
+//
+//  YourNameView.swift
+//  StrategicTrio
+//
+//  Created by Aakash Ambodkar on 8/4/24.
+//
+
+import SwiftUI
+
+struct YourNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YourNameView()
+}
